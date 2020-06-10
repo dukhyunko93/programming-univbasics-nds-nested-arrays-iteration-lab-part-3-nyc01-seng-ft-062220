54cbd3c09070e284f431d0d0bf1ec483aa41a478
Duke Ko <dukhyunko93@gmail.com>
2020-06-10 16:22:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,10 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = [0]
     string = " "
-    while element_index < src[row_index].count do
+    while element_index < src[row_index].counut do
+      if src[row_index][element_index] = " "
+        src[row_index][element_index] = string
+      end
       element_index += 1
     end
     join_nested_strings << string
